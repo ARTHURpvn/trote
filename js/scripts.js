c263abfb22c4c0eec9f027df71d1eed7fbad8483
ARTHURpvn <arthursppavan@gmail.com>
2025-03-27 20:31:00 -0300

    terminando pagina de gincanas

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 function calcular() {
     let mascote = document.getElementById("mascote").value;
     let homenagem = document.getElementById("homenagem").value;
-    let som = parseFloat(mascote) + parseFloat(homenagem)
-    alert("funcionou "+ som);
+    let leite = document.getElementById("leite").value;
+    let kitAvulso = document.getElementById("kitAvulso").value
+    let suplementoAvulso = document.getElementById("suplementoAvulso").value
+
+    let soma = parseFloat(mascote) + parseFloat(homenagem) + (2* parseFloat(leite)) + (30 * parseFloat(kitAvulso)) + (15 * parseFloat(suplementoAvulso))
+    let label = document.getElementById("soma");
+
+    label.innerHTML =  `Soma total: ${soma}`
 }
