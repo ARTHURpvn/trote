7a619e9d46e4ef3d920d9fe89b5c180af9b47818
ARTHURpvn <arthursppavan@gmail.com>
2025-04-01 22:15:07 -0300

    adicionando algumas funcionalidades na calculadora

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,31 @@
 function calcular() {
-    let mascote = document.getElementById("mascote").value;
-    let homenagem = document.getElementById("homenagem").value;
-    let leite = document.getElementById("leite").value;
-    let kitAvulso = document.getElementById("kitAvulso").value
-    let suplementoAvulso = document.getElementById("suplementoAvulso").value
-
-    let soma = parseFloat(mascote) + parseFloat(homenagem) + (2* parseFloat(leite)) + (30 * parseFloat(kitAvulso)) + (15 * parseFloat(suplementoAvulso))
+    let mascote = Number(document.getElementById("mascote").value);
+    let homenagem = Number(document.getElementById("homenagem").value);
+    let leite = Number(document.getElementById("leite").value);
+    let kit = Number(document.getElementById("kit").value);
+    let suplemento = Number(document.getElementById("suplemento").value);
     let label = document.getElementById("soma");
+    let equipe = document.getElementById("equipe").value;
+
+    let soma = mascote + homenagem + (2* leite)
+    
+    if(equipe == "Laranja") {
+        if(kit >= 97) {
+            soma += 5000 + ((kit - 97) * 30)
+
+        } else if(kit >= 78) {
+            soma += 4000 + ((kit - 78) * 30)
+
+        } else if(kit >= 49) {
+            soma += 2500 + ((kit - 48) * 30)
+
+        } else if(kit >= 19) {
+            soma += 1000 + ((kit - 19) * 30)
+            
+        }
+
+    }
+    
 
     label.innerHTML =  `Soma total: ${soma}`
 }
