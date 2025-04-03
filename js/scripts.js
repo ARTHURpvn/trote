d23640c933236d52ce910e2749700bbeaa71a8e2
ARTHURpvn <arthursppavan@gmail.com>
2025-04-03 20:31:38 -0300

    mudando estilo do header + adicionando mais funcoes na calculadora

--- js/scripts.js
+++ js/scripts.js
@@ -10,17 +10,17 @@ function calcular() {
     let soma = mascote + homenagem + (2* leite)
     
     if(equipe == "Laranja") {
-        if(kit >= 97) {
-            soma += 5000 + ((kit - 97) * 30)
+        if(kit >= 97 && suplemento >= 49) {
+            soma += 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15)
 
-        } else if(kit >= 78) {
-            soma += 4000 + ((kit - 78) * 30)
+        } else if(kit >= 78 && suplemento >= 39) {
+            soma += 4000 + ((kit - 78) * 30) + ((suplemento - 39) * 15)
 
-        } else if(kit >= 49) {
-            soma += 2500 + ((kit - 48) * 30)
+        } else if(kit >= 49 && suplemento >= 25) {
+            soma += 2500 + ((kit - 48) * 30) + ((suplemento - 25) * 15)
 
-        } else if(kit >= 19) {
-            soma += 1000 + ((kit - 19) * 30)
+        } else if(kit >= 19 && suplemento >= 10) {
+            soma += 1000 + ((kit - 19) * 30) + ((suplemento - 10) * 15)
             
         }
 
